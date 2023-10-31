81d0b53ee1adb9a8f5c50c04023704025da55fde
ainasorfina01 <148048906+ainasorfina01@users.noreply.github.com>
2023-10-31 15:39:37 +0800

    Update SliderData.jsx

--- components/SliderData.jsx
+++ components/SliderData.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-// Import the individual image files
-import img1 from '../public/IMG_4601.jpg';
-import img2 from '../public/IMG_2023.JPG';
-import img3 from '../public/IMG_0530.jpg';
-import img4 from '../public/IMG_4599.jpg';
-import img5 from '../public/IMG_0539.jpg';
-
-// Create an array of image variables
-const images = [img1, img2, img3, img4, img5];
-
-// Create the SliderData array
-export const SliderData = images.map((image, index) => ({
-  image
-  // You can add other properties if needed, such as alt text.
-  // alt: `Image ${index + 1}`,
-}));
+export const SliderData = [
+ 
+  {
+    image: 'public/IMG_4601.jpg',
+  },
+  {
+    image: 'public/IMG_2023.JPG',
+  },
+  {
+    image: 'public/IMG_0530.jpg',
+  },
+  {
+    image: 'public/IMG_4599.jpg',
+  },
+  {
+    image: 'public/IMG_0539.jpg',
+  },
+];

e3ea18c8f80921ccfb1ad705057ba177b396d4d9
ainasorfina01 <148048906+ainasorfina01@users.noreply.github.com>
2023-10-31 15:03:41 +0800

    Update SliderData.js

--- components/SliderData.js
+++ components/SliderData.js
@@ -1,18 +1,18 @@
 export const SliderData = [
  
   {
-    image: '/IMG_4601.jpg',
+    image: './public/IMG_4601.jpg',
   },
   {
-    image: '/IMG_2023.JPG',
+    image: './public/IMG_2023.JPG',
   },
   {
-    image: '/IMG_0530.jpg',
+    image: './public/IMG_0530.jpg',
   },
   {
-    image: '/IMG_4599.jpg',
+    image: './public/IMG_4599.jpg',
   },
   {
-    image: '/IMG_0539.jpg',
+    image: './public/IMG_0539.jpg',
   },
 ];

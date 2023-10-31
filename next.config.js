58189e9a761a4de55ae53ab64e8e639bf562d4a3
ainasorfina01 <148048906+ainasorfina01@users.noreply.github.com>
2023-10-31 09:58:01 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
-};
+  reactStrictMode: true,
+}
+
+module.exports = {
+  images: {
+    domains: [
+      'images.unsplash.com',
+    ]
+  }
+}

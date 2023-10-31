5b0b205f8821faae64440ede77f8b30f95bf273c
ainasorfina01 <148048906+ainasorfina01@users.noreply.github.com>
2023-10-31 15:51:13 +0800

    Update SliderData.jsx

--- components/SliderData.jsx
+++ components/SliderData.jsx
@@ -1,18 +1,18 @@
 export const SliderData = [
  
   {
-    image: 'public/IMG_4601.JPG',
+    image: '/IMG_4601.JPG',
   },
   {
-    image: 'public/IMG_2023.JPG',
+    image: '/IMG_2023.JPG',
   },
   {
-    image: 'public/IMG_0530.JPG',
+    image: '/IMG_0530.JPG',
   },
   {
-    image: 'public/IMG_4599.JPG',
+    image: '/IMG_4599.JPG',
   },
   {
-    image: 'public/IMG_0539.JPG',
+    image: '/IMG_0539.JPG',
   },
 ];
